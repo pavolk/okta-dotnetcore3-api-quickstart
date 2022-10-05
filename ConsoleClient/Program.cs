bbac0bbe3a362eda727ecb2353c9c517c0bba2d1
Pavol Kurina <pavol.kurina@gmail.com>
2022-10-05 16:08:21 +0200

    added console client

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Webapi;
+
+namespace ConsoleClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+
+            try {
+                var c = new Client("https://localhost:5001", httpClient);
+                var result = c.HelloAsync().GetAwaiter().GetResult();
+            } catch (Exception ex) {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+    }
+}
